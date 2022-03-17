28d65b19a8c584d96d8efde6bfbaab876c4bb2dd
Alexandr-77 <62809713+Alexandr-77@users.noreply.github.com>
2022-03-17 18:22:14 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,10 +5,6 @@ let nav = document.querySelector('.header__block-nav');
 let hideBox = document.querySelector('.popular__box-hide');
 let btnPopular = document.querySelector('.popular__btn');
 
-let btnCallTop = document.querySelector('.footer__link-call');
-let btnCall = document.querySelector('.call--mi');
-let popap = document.querySelector('.popap');
-let popapClose = document.querySelector('.popap__close');
 
 // ======================================================
 
@@ -31,21 +27,43 @@ btnPopular.addEventListener('click', function() {
 
 // ======================
 
-btnCallTop.addEventListener('click', functionCall);
-function functionCall() {
-  popap.style.display = "block";
-}
 
-btnCall.addEventListener('click', functionCall2);
-function functionCall2() {
-  popap.style.display = "block";
-}
+let btnCall = document.querySelectorAll('.call--mi');
+let popap = document.querySelector('.popap');
+let popapClose = document.querySelector('.popap__close');
 
-popapClose.addEventListener('click', functionCall3);
-function functionCall3() {
+popapClose.addEventListener('click', function () {
   popap.style.display = "none";
+});  
+
+for (let i = 0; i < btnCall.length; i++) {
+  btnCall[i].addEventListener('click', functionCall2);  
 }
 
+function functionCall2() {
+    popap.style.display = "block";
+  }
+
+// ====================================
+let checketMs = document.querySelector('#popap-for');
+let otpBtn = document.querySelector('.popap__btn');
+
+let popapBlago = document.querySelector('.popap__blago');
+
+checketMs.addEventListener('click', function () {
+  if (checketMs.checked) {
+    otpBtn.style.backgroundColor = "var(--color-bg)";
+    otpBtn.style.cursor = "pointer";
+  } else {
+    otpBtn.style.background = "var(--color-xs)";
+    otpBtn.style.cursor = "default";
+  } 
+});  
+
+otpBtn.addEventListener('click', function() {
+  popap.style.display = "none";
+  popapBlago.style.display = "block";
+});
 
 // ====================================================
 
